35fdd4bb1e1627f91eb8bcc659d4a393f6adb138
Mike <007mrice@gmail.com>
2025-03-22 23:09:54 -0500

    Add password validator code

---
+++ password-validator.js
@@ -0,0 +1,22 @@
+const readlineSync = require('readline-sync');
+
+function isValidPassword(password) {
+  const hasUpperCase = /[A-Z]/.test(password);
+  const hasNumber = /[0-9]/.test(password);
+  const isLongEnough = password.length >= 8;
+
+  return hasUpperCase && hasNumber && isLongEnough;
+}
+
+let password;
+
+do {
+  password = readlineSync.question('Enter a password: ');
+
+  if (isValidPassword(password)) {
+    console.log('✅ Password accepted!');
+  } else {
+    console.log('❌ Password must be at least 8 characters long, include a number, and an uppercase letter.\n');
+  }
+
+} while (!isValidPassword(password));
